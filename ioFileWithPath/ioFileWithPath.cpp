68928bf4d72e5ad8fa3a8d5c85274ba3d15484b0
syairaarif <putrisyafira4444@gmail.com>
2024-06-04 10:49:16 +0700

    membuat program untuk membuka file dalam mode membaca

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -32,6 +32,27 @@ int main()
     //selesai dalam menulis sekarang tutup file
     outfile.close();
 
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 
 }
 
