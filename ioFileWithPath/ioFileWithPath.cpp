feca6005e17a07b0a4a36707b28e9c06bf864444
syairaarif <putrisyafira4444@gmail.com>
2024-06-04 10:32:58 +0700

    membuat variabl baris dan namafile

---
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -0,0 +1,16 @@
+#include<fstream>
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string baris;
+    string NamaFile;
+
+    cout << "Masukan nama file : ";
+    cin >> NamaFile;
+
+
+}
+
