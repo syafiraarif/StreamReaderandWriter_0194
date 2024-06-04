de0e98346e2d033cc563ccd85ee842543251e0b7
syairaarif <putrisyafira4444@gmail.com>
2024-06-04 10:34:26 +0700

    membuat program membuka file dalam mode menulis

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -11,6 +11,11 @@ int main()
     cout << "Masukan nama file : ";
     cin >> NamaFile;
 
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
 
+    cout << ">= menulis file, \'q\' untuk keluar" << endl;
 }
 
