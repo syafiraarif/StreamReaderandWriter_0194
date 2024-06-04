a47c5c1c2d03eabfd5f33dbd9f82aa25b49348eb
syairaarif <putrisyafira4444@gmail.com>
2024-06-04 10:26:37 +0700

    membuat program untuk membaca file jika file yang ingin dibaca ada

--- StreamReaderandWriter/StreamReaderandWriter.cpp
+++ StreamReaderandWriter/StreamReaderandWriter.cpp
@@ -35,5 +35,22 @@ int main()
     ifstream infile;
     //menunjuk ke sebuah file
     infile.open("contohfile.txt");
+
+    cout << endl << ">= membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
 
