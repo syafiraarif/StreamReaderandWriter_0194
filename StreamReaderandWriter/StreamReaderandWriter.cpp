458e72940e4c3e76fc4a92d34a172548809a2fe7
syairaarif <putrisyafira4444@gmail.com>
2024-06-04 10:15:54 +0700

    membuka dan menutup file dalam mode menulis

--- StreamReaderandWriter/StreamReaderandWriter.cpp
+++ StreamReaderandWriter/StreamReaderandWriter.cpp
@@ -7,6 +7,28 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //untuk menyimpan data berbentuk string
+    string baris;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimeted loop untuk menulis
+    while(true)
+    {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q")break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup file
+    outfile.close();
 }
 
